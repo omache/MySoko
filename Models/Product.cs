7289c19d091c47bf8cc540bccad4312951a5305a
Qwellstra <109847941+Qwellstra@users.noreply.github.com>
2024-08-28 11:54:46 +0300

    Adjusted the positioning of the images in the homepage

--- Models/Product.cs
+++ Models/Product.cs
@@ -1,15 +1,22 @@
-﻿namespace MySoko.Models
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace MySoko.Models
 {
     public class Product
     {
-        public int ProductId { get; set; }         
-        public string? Name { get; set; }           
-        public string? Description { get; set; }   
-        public string? Category { get; set; }       
+        public int ProductId { get; set; }
+        public string? Name { get; set; }
+        public string? Description { get; set; }
+        public string? Category { get; set; }
 
         // Pricing and Inventory
-        public decimal Price { get; set; }        
+        [DisplayFormat(DataFormatString = "{0:C}", ApplyFormatInEditMode = false)]
+        public decimal Price { get; set; }
+        [Display(Name = "Stock Quantity")]
         public int? StockQuantity { get; set; }
         public byte[]? ItemPhoto { get; set; }
     }
+
+
 }
