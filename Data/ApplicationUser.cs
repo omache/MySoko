17d2dbe4bdb24978df867da9eff44331b8d6770b
Qwellstra <109847941+Qwellstra@users.noreply.github.com>
2024-09-02 13:45:10 +0300

    Want to scaffol the admin data

--- Data/ApplicationUser.cs
+++ Data/ApplicationUser.cs
@@ -5,7 +5,15 @@ namespace MySoko.Data
     public class ApplicationUser : IdentityUser
     {
         public string FirstName { get; set; }
-        public string LastName { get; set; } 
+        public string LastName { get; set; }
         public byte[]? ProfilePhoto { get; set; }
+        public Roles? UserRoles { get; set; } = Roles.Customer;
+    }
+
+    public enum Roles
+    {
+        Admin,
+        Merchant,
+        Customer
     }
 }
---
+++ Data/Migrations/20240902102758_RolesAdded.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MySoko.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class RolesAdded : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "UserRoles",
+                table: "AspNetUsers",
+                type: "int",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "UserRoles",
+                table: "AspNetUsers");
+        }
+    }
+}
