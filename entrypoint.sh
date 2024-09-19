e426bcd3fac28d70d3fcbdbc942de242d5d564f7
omache <75061755+omache@users.noreply.github.com>
2024-09-19 11:24:28 +0300

    Delete entrypoint.sh

--- entrypoint.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-set -e
-
-# Run migrations
-dotnet ef database update --project YourProject.csproj --startup-project YourStartupProject.csproj
-
-# Start the application
-exec dotnet MySoko.dll
