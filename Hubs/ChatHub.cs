37d7b0e496a7e22fa619826168c7e877a8158821
omache <75061755+omache@users.noreply.github.com>
2024-09-23 07:03:17 +0000

    more

--- Hubs/ChatHub.cs
+++ Hubs/ChatHub.cs
@@ -11,10 +11,13 @@ namespace MySoko.Hubs
     public class ChatHub : Hub
     {
         private readonly UserManager<ApplicationUser> _userManager;
+        private readonly SignInManager<ApplicationUser> _signInManager;
 
-        public ChatHub(UserManager<ApplicationUser> userManager)
+
+        public ChatHub(UserManager<ApplicationUser> userManager, SignInManager<ApplicationUser> signInManager)
         {
             _userManager = userManager;
+            _signInManager = signInManager;
         }
 
         public async Task SendMessage(string user, string message)
@@ -29,26 +32,9 @@ namespace MySoko.Hubs
                     await Clients.User(appUser.Id).SendAsync("ReceiveMessage", user, message);
                 }
             }
+            await Clients.Caller.SendAsync("ReceiveMessage", user, message);
 
-            // Optionally, you can notify the admin with a list of users who sent messages
-            var userList = appUsers
-                .Where(u => u.UserRoles != "Admin")
-                .Select(u => new { u.Id, u.FirstName })
-                .ToList();
-
-            foreach (var appUser in appUsers)
-            {
-                if (appUser.UserRoles == "Admin")
-                {
-                    await Clients.User(appUser.Id).SendAsync("ReceiveUserList", userList);
-                }
-            }
         }
-
-        public async Task ReplyToUser(string userId, string message)
-        {
-            // Send reply from admin to specific user
-            await Clients.User(userId).SendAsync("ReceiveMessage", "Admin", message);
-        }
+        
     }
 }
